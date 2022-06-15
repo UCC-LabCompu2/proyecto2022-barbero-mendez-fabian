e6745d1fe092e85f3f7d031db86e4b1deb4c124c
MaxiBarberoo <2104098@ucc.edu.ar>
2022-06-14 23:33:14 -0300

    Cambios Finales

--- MisFunciones.js
+++ MisFunciones.js
@@ -1,6 +1,7 @@
-
-var auto1 = document.getElementById('Colores1');
-var auto2 = document.getElementById('Colores2');
+/**
+ * Imprimir en el canvas la pista al cargar la pagina
+ * @method IniciarPista
+ */
 
 function IniciarPista(){
     var canvas = document.getElementById("miCanvas");
@@ -11,4 +12,97 @@ function IniciarPista(){
 
     contexto.drawImage(Pista, 175, 200);
     contexto.drawImage(Pista, 175, 0);
+}
+
+/**
+ * Dibuja Los Autos de los colores especificos
+ * @method DibujarAutos
+ */
+function DibujarAutos(){
+    var canvas = document.getElementById("miCanvas");
+    var contexto = canvas.getContext("2d");
+
+    canvas.width = canvas.width;
+
+    var Pista = new Image();
+    Pista.src = 'Imagenes/FondoPista.jpeg';
+
+    contexto.drawImage(Pista, 175, 200);
+    contexto.drawImage(Pista, 175, 0);
+
+    var auto1 = document.getElementById("Colores1");
+    var auto2 = document.getElementById("Colores2");
+
+    var HP1 = document.getElementById("HP1").value;
+    var HP2 = document.getElementById("HP2").value;
+    var Peso1 = document.getElementById("Peso1").value;
+    var Peso2 = document.getElementById("Peso2").value;
+
+    var AutoBlanco = new Image();
+    AutoBlanco.src = "Imagenes/AutoBlanco.png";
+
+    var AutoRojo = new Image();
+    AutoRojo.src = "Imagenes/AutoRojo.png";
+
+    var AutoAzul = new Image();
+    AutoAzul.src = "Imagenes/AutoAzul.png";
+
+    var AutoVerde = new Image();
+    AutoVerde.src = "Imagenes/AutoVerde.png";
+
+    if(auto1.selectedOptions[0].value === "blanco"){
+        contexto.drawImage(AutoBlanco, 205, 530, 50, 100);
+    }else if(auto1.selectedOptions[0].value === "azul"){
+        contexto.drawImage(AutoAzul, 205, 530, 50, 100);
+    }else if(auto1.selectedOptions[0].value === "verde"){
+        contexto.drawImage(AutoVerde, 205, 530, 50, 100);
+    }else if(auto1.selectedOptions[0].value === "rojo"){
+        contexto.drawImage(AutoRojo, 205, 530, 50, 100);
+    }
+
+    if(auto2.selectedOptions[0].value === "blanco"){
+        contexto.drawImage(AutoBlanco, 320, 530, 50, 100);
+    }else if(auto2.selectedOptions[0].value === "azul"){
+        contexto.drawImage(AutoAzul, 320, 530, 50, 100);
+    }else if(auto2.selectedOptions[0].value === "verde"){
+        contexto.drawImage(AutoVerde, 320, 530, 50, 100);
+    }else if(auto2.selectedOptions[0].value === "rojo"){
+        contexto.drawImage(AutoRojo, 320, 530, 50, 100);
+    }
+}
+
+/**
+ * Verifica Los inputs del formulario
+ * @method VerificarFormulario
+ */
+function VerificarFormulario(){
+
+    var HP1 = document.getElementById("HP1").value;
+    var HP2 = document.getElementById("HP2").value;
+    var Peso1 = document.getElementById("Peso1").value;
+    var Peso2 = document.getElementById("Peso2").value;
+
+    if(HP1 === "" || HP1 < 0){
+        document.getElementById("HP1").value = 0;
+        alert("Complete el formulario");
+        return false;
+    }
+
+    if(HP2 === "" || HP2 < 0){
+        document.getElementById("HP2").value = 0;
+        alert("Complete el formulario");
+        return false
+    }
+
+    if(Peso1 === "" || Peso1 <= 0){
+        document.getElementById("Peso1").value = 800;
+        alert("Complete el formulario");
+        return false
+    }
+
+    if(Peso2 === "" || Peso2 <= 0){
+        document.getElementById("Peso2").value = 800;
+        alert("Complete el formulario");
+        return false
+    }
 }
